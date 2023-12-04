d3084e5565f79cf51f0cbc0e35ccbaa8023635d0
yonseiSY <148183644+yonseiSY@users.noreply.github.com>
2023-12-04 19:19:20 +0900

    2023-12-04
    Change Debug.Log

--- Branch_A.cs
+++ Branch_A.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Branch_A : MonoBehaviour {
     void Start()
     {
-        Debug.Log("This is a A Branch.");
+        Debug.Log("This is a not A Branch.");
     }
 
     void Update()
