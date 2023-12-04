e5fd1718a472683222c6654736f06aeb3f538c81
yonseiSY <148183644+yonseiSY@users.noreply.github.com>
2023-12-04 19:28:12 +0900

    2023-12-04
    Change Script

--- Hot_Fix.cs
+++ Hot_Fix.cs
@@ -5,12 +5,6 @@ using UnityEngine;
 public class Hot_Fix : MonoBehaviour {
     void Start()
     {
-        Debug.Log("The Error is solved.");
-        Debug.Log("The other Error is solved.");
-    }
-
-    void Update()
-    {
-
+        Debug.Log("There is no error");
     }
 }
