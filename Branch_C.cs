ad365ea1a904dee30a68192d7eee6712a2e4bdd8
ESLAB-258 <eslab258@gmail.com>
2023-12-04 20:23:48 +0900

    2023-12-04
    Add "Branch_C.cs"

---
+++ Branch_C.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Branch_C : MonoBehaviour {
+    void Start()
+    {
+        Debug.Log("This is a C Branch.");
+    }
+
+    void Update()
+    {
+        
+    }
+}
