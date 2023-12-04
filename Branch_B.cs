ca6f484e3017eeaa22bf117b7d3ab3b4015f9f18
yonseiSY <148182747+ESLAB-258@users.noreply.github.com>
2023-12-04 20:10:45 +0900

    2023-12-04
    Add "Branch_B.cs"

---
+++ Branch_B.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collectioins.Generic;
+using UnityEngine;
+
+public class Branch_B : MonoBehaviour {
+    void Start()
+    {
+        Debug.Log("This is a B Branch.");
+    }
+
+    void Update()
+    {
+
+    }
+}
