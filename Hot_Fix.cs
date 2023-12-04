94972c102469ca4583d383136cfcd1f7cbfe810d
yonseiSY <148183644+yonseiSY@users.noreply.github.com>
2023-12-04 19:09:50 +0900

    2023-12-04
    Add "Hot_Fix.cs"

---
+++ Hot_Fix.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hot_Fix : MonoBehaviour {
+    void Start()
+    {
+        Debug.Log("The Error is solved.");
+    }
+
+    void Update()
+    {
+
+    }
+}
