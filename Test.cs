9c8c2a96c639e19d5c3e09189dd6861fbb7b1fb7
yonseiSY <148183644+yonseiSY@users.noreply.github.com>
2023-12-04 18:43:10 +0900

    2023-12-04
    Add "Test.cs"

---
+++ Test.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+    void Start()
+    {
+        Debug.Log("This is a Stage(Main) branch.");
+    }
+
+    void Update()
+    {
+
+    }
+}
